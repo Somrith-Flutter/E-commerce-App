1e9724f1c881ae1e6b180c2c33ddbbd2b900bd4f
Chhorn Leanghor <chhornleanghor0@gmail.com>
2024-08-27 15:10:52 +0700

    dark mode and change name app

---
+++ lib/app/controllers/theme_controller.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeController extends GetxController implements GetxService {
+  Rx<ThemeMode> currentTheme = ThemeMode.system.obs;
+
+  void switchTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    currentTheme.value = currentTheme.value == ThemeMode.light
+        ? ThemeMode.dark
+        : ThemeMode.light;
+
+    await prefs.setBool('isDarkMode', currentTheme.value == ThemeMode.dark);
+  }
+
+  Future<void> loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isDarkMode = prefs.getBool('isDarkMode') ?? false;
+    currentTheme.value = !isDarkMode ? ThemeMode.dark : ThemeMode.light;
+  }
+}
